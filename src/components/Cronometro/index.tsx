4b023384f7b53f98aca4b7a455525f4094bb4848
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-02-22 18:03:53 -0300

    feat: implementa tempo

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,27 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { ITarefa } from "../../types/Tarefa";
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/Time";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado?.tempo));
+    }
+  }, [selecionado]);
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <p>Tempo: {tempo}</p>
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
