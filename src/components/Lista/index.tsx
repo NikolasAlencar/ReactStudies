af2f4c0257efaa47361ecfb2c135cd9ec762ed32
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-02-22 17:27:41 -0300

    feat: implementa seleção de tarefa

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -3,13 +3,18 @@ import style from "./Lista.module.scss";
 import Item from "./Item";
 import { ITarefa } from "../../types/Tarefa";
 
-function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+interface Props {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item) => (
+          <Item key={item.id} {...item} selecionaTarefa={selecionaTarefa} />
         ))}
       </ul>
     </aside>
